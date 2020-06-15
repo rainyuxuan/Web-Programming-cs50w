15c3cffcaa0b98bdd00bdc2ab0ce023b9a9410a0
rainyuxuan <yu2001xuan@outlook.com>
2020-06-14 21:24:59 -0400

    start project2; finished nav template

---
+++ project2/app.py
@@ -0,0 +1,31 @@
+import os
+from pip._vendor import requests
+
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+USER = None
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
