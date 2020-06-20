ae69aff4eae6047aaac51c7563522daa59e9887e
rainyuxuan <yu2001xuan@outlook.com>
2020-06-19 20:31:18 -0400

    finished majority project2

--- project2/app.py
+++ project2/app.py
@@ -61,8 +61,8 @@ USER = None
 def index():
     # session.clear()
     print("OPEN")
-    fav_channels = db.execute('SELECT * FROM channels WHERE id < 5 ORDER BY id ASC').fetchall()
-    explore_channels = db.execute('SELECT * FROM channels WHERE id > 4 ORDER BY id ASC').fetchall()
+    fav_channels = db.execute('SELECT * FROM channels WHERE id < 6 ORDER BY id ASC').fetchall()
+    explore_channels = db.execute('SELECT * FROM channels WHERE id > 5 ORDER BY id ASC').fetchall()
     db.commit()
     return render_template('index.html', favChannels=fav_channels, channels=explore_channels)
 
@@ -72,6 +72,8 @@ def channel(channel_id):
     channel = db.execute("SELECT * FROM channels WHERE id = :id", {'id':channel_id}).fetchone();
     if channel is None:
         return f"Error 404: Stelo #{channel_id} not Found"
+    # build message list
+    delete_messages(channel_id)
     messages = db.execute("SELECT * FROM messages WHERE channel_id = :channel_id ORDER BY id ASC", {'channel_id':channel_id}).fetchall();
     db.commit()
     # print(messages)
@@ -112,6 +114,7 @@ def create(data):
     emit("announce channel", {"channel_name":channel_name, "channel_id": channel_id, 'creator': data['creator']}, broadcast=True)
 
 
+
 @socketio.on("send message")
 def send(data):
     # data = {'channel', 'name', 'time', 'content'}
@@ -121,9 +124,25 @@ def send(data):
     time = data['time']
     content = data['content']
     db.execute('INSERT INTO messages (channel_id, name, time, content) VALUES (:channel_id, :name, :time, :content)', {'channel_id':channel_id, 'name':name, 'time':time, 'content':content})
+    # Delete extra messages of this channel
+    delete_messages(channel_id)
+        
     db.commit()
     print(f"Inserted message: #{channel_id} {name}@{time}: {content}")
     emit("post message", data, broadcast=True)
+
+
+
+def delete_messages(channel_id):
+    count = db.execute('SELECT COUNT(*) FROM messages WHERE channel_id = :channel_id', {'channel_id': channel_id}).fetchone()[0]
+    if count > 5:
+        num_delete = count - 5
+        delete_targets = db.execute('SELECT id FROM messages WHERE channel_id = :channel_id ORDER BY id ASC LIMIT :num', {'channel_id': channel_id, 'num': num_delete}).fetchall()
+        print(delete_targets)
+        print(channel_id)
+        for target in delete_targets:
+            print(target[0])
+            db.execute('DELETE FROM messages WHERE id= :id', {'id': target[0]})
 
 
 if __name__ == '__main__': 
